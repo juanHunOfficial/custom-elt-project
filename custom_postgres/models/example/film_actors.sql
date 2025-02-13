7cf51386f2b32984ddaea8a9d496ccbad9b41d76
JUAN_HUN <juanhun2424@gmail.com>
2025-02-13 12:55:04 -0500

    Add first dbt project

---
+++ custom_postgres/models/example/film_actors.sql
@@ -0,0 +1,5 @@
+SELECT * FROM {{ source('destination_db', 'film_actors')}}
+-- What this line is doing is using 'destination_db' as the source and grabbing the 'film_actors' table
+-- These are what's known as references (films.sql, actors.sql, film_actors.sql) that we use to create our custom models. 
+-- The next part is the schema.yml file, thats where your custom models are so you need to specify what the models will look like.
+-- The schema will define what this data should look like, this is useful for testing your data and pipeline. 
